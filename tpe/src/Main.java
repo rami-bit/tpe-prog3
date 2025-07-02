1f6136aa9ead73ad9fa0e7dab8ce9d17beb0e22b
rami-bit <ramidv12@gmail.com>
2025-07-02 15:07:13 -0300

    borro parte del scanner cuando scanneaba dentro del main.

--- tpe/src/Main.java
+++ tpe/src/Main.java
@@ -1,10 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -35,28 +29,5 @@ public class Main {
         }
     }
 
-    public static Fabrica construirEmpresa() {
-        int piezasTotales = 0;
-        List<Maquina> maquinas = new ArrayList<>();
-        boolean esPrimerLinea = true;
-        System.out.println(new File(".").getAbsolutePath());
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(".\\tpe\\\\data\\prueba.txt"))){
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (esPrimerLinea) {
-                    piezasTotales = Integer.parseInt(line);
-                } else {
-                    String[] configMaquina = line.split(",");
-                    Maquina m = new Maquina(configMaquina[0], Integer.parseInt(configMaquina[1]));
-                    maquinas.add(m);
-                }
-                esPrimerLinea = false;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return new Fabrica(maquinas, piezasTotales);
-    }
+    
 }
